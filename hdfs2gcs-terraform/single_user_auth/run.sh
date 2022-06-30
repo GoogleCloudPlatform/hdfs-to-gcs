bf7bfdc7f94e3962add3f18da7a4a686e6cabb2b
Sa Af <saeedeh.afshari@gmail.com>
2022-06-30 15:38:54 -0700

    Update run.sh

--- hdfs2gcs-terraform/single_user_auth/run.sh
+++ hdfs2gcs-terraform/single_user_auth/run.sh
@@ -16,26 +16,26 @@
 
 # Running terraform scripts with storing required files in GCS bucket 
 BUCKET_NAME=$1
-gsutil cp -r ../resource/* gs://$BUCKET_NAME
-mkdir -p ../binaries
+gsutil cp -r ../../resource/* gs://$BUCKET_NAME
+mkdir -p ../../binaries
 # unzip tool
-curl -fSL "http://ftp.de.debian.org/debian/pool/main/u/unzip/unzip_6.0-23+deb10u2_amd64.deb" -o ../binaries/unzip_6.0-23+deb10u2_amd64.deb
-curl -fSL "http://mirror.centos.org/centos/7/os/x86_64/Packages/unzip-6.0-21.el7.x86_64.rpm" -o ../binaries/unzip-6.0-21.el7.x86_64.rpm
+curl -fSL "http://ftp.de.debian.org/debian/pool/main/u/unzip/unzip_6.0-23+deb10u2_amd64.deb" -o ../../binaries/unzip_6.0-23+deb10u2_amd64.deb
+curl -fSL "http://mirror.centos.org/centos/7/os/x86_64/Packages/unzip-6.0-21.el7.x86_64.rpm" -o ../../binaries/unzip-6.0-21.el7.x86_64.rpm
 
 # nifi 
-curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-1.15.3-bin.zip" -o ../binaries/nifi-1.15.3-bin.zip
+curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-1.15.3-bin.zip" -o ../../binaries/nifi-1.15.3-bin.zip
 
 # nifi toolkit
-curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-toolkit-1.15.3-bin.zip" -o ../binaries/nifi-toolkit-1.15.3-bin.zip
+curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-toolkit-1.15.3-bin.zip" -o ../../binaries/nifi-toolkit-1.15.3-bin.zip
 
 # zookeeper
-curl -fSL "https://downloads.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz" -o ../binaries/apache-zookeeper-3.8.0-bin.tar.gz
+curl -fSL "https://downloads.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz" -o ../../binaries/apache-zookeeper-3.8.0-bin.tar.gz
 
 # jdk 11
-curl -fSL "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz" -o ../binaries/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz
+curl -fSL "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz" -o ../../binaries/OpenJDK11U-jdk_x64_linux_hotspot_11.0.15_10.tar.gz
 
 # Copying resource folder to GCS bucket
-gsutil cp -r ../binaries/* gs://$BUCKET_NAME/binaries
+gsutil cp -r ../../binaries/* gs://$BUCKET_NAME/binaries
 
 # If you are using an existing network & subnetwork, comment all the lines in network.tf and firewall.tf and use the network id of your own network OR shared VPC
 # in network_interface block in nifi.tf, nifi-ca.tf & zookeeper.tf with the below format:
