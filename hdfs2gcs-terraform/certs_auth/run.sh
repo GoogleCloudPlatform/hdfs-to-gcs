44ae36386247d96dc0b7c0834c5bd51b515201f5
Sa Af <saeedeh.afshari@gmail.com>
2022-06-30 15:36:43 -0700

    Update run.sh

--- hdfs2gcs-terraform/certs_auth/run.sh
+++ hdfs2gcs-terraform/certs_auth/run.sh
@@ -19,20 +19,20 @@ BUCKET_NAME=$1
 gsutil cp -r ../../resource/* gs://$BUCKET_NAME
 mkdir -p ../../binaries
 # unzip tool
-curl -fSL "http://ftp.de.debian.org/debian/pool/main/u/unzip/unzip_6.0-23+deb10u2_amd64.deb" -o ../binaries/unzip_6.0-23+deb10u2_amd64.deb
-curl -fSL "http://mirror.centos.org/centos/7/os/x86_64/Packages/unzip-6.0-21.el7.x86_64.rpm" -o ../binaries/unzip-6.0-21.el7.x86_64.rpm
+curl -fSL "http://ftp.de.debian.org/debian/pool/main/u/unzip/unzip_6.0-23+deb10u2_amd64.deb" -o ../../binaries/unzip_6.0-23+deb10u2_amd64.deb
+curl -fSL "http://mirror.centos.org/centos/7/os/x86_64/Packages/unzip-6.0-21.el7.x86_64.rpm" -o ../../binaries/unzip-6.0-21.el7.x86_64.rpm
 
 # nifi 
-curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-1.15.3-bin.zip" -o ../binaries/nifi-1.15.3-bin.zip
+curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-1.15.3-bin.zip" -o ../../binaries/nifi-1.15.3-bin.zip
 
 # nifi toolkit
-curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-toolkit-1.15.3-bin.zip" -o ../binaries/nifi-toolkit-1.15.3-bin.zip
+curl -fSL "https://archive.apache.org/dist/nifi/1.15.3/nifi-toolkit-1.15.3-bin.zip" -o ../../binaries/nifi-toolkit-1.15.3-bin.zip
 
 # zookeeper
-curl -fSL "https://downloads.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz" -o ../binaries/apache-zookeeper-3.8.0-bin.tar.gz
+curl -fSL "https://downloads.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz" -o ../../binaries/apache-zookeeper-3.8.0-bin.tar.gz
 
 # jdk 11
-curl -fSL "https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz" -o ../binaries/openjdk-11+28_linux-x64_bin.tar.gz
+curl -fSL "https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz" -o ../../binaries/openjdk-11+28_linux-x64_bin.tar.gz
 
 # Copying resource folder to GCS bucket
 gsutil cp -r ../../binaries/* gs://$BUCKET_NAME/binaries
